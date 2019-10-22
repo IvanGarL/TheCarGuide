49ad584736e4ce168e1532d3087d60fe2efbb472
Leidy Romero <lj.romero@uniandes.edu.co>
2019-10-22 08:25:38 -0500

    components

---
+++ car-guide-back/car-guide-front/src/Components/Navegacion.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+//import { useAuth0 } from "../react-auth0-wrapper";
+import { Auth0Context } from "../react-auth0-wrapper";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {NavLink}  from 'react-router-dom'
+import '../app.css';
+
+
+class Navegacion extends Component {
+
+    static contextType = Auth0Context;
+
+    render(){
+
+      const { isAuthenticated, loginWithRedirect, logout } = this.context;
+
+      return(
+
+      <nav className="navbar navbar-expand-md navbar-light sticky-top">
+          <a ><img src="bkt.PNG" height="28" alt="BKT"/></a>
+          <a className="navbar-brand"><h3 className="text-dark">BKT</h3></a>
+
+          <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+              <div className="navbar-nav ml-auto">
+              <NavLink to="/" > 
+                <a className="a nav-item nav-link" id="btn-inicio">Inicio</a>
+              </NavLink>
+              <NavLink to="/cars">
+                <a  className="a nav-item nav-link" id="btn-cars">Comprar</a>
+              </NavLink>
+              <NavLink to="/services" >
+                <a className="a nav-item nav-link" id="btn-services">Servicios</a>
+              </NavLink>
+                <a className="a nav-item nav-link" id="btn-comparador">Comparador</a>
+                {!isAuthenticated && (<button className="nav-item nav-link nav-pills" id="btn-login" onClick={() => loginWithRedirect({})}>Iniciar Sesión</button>)}
+                {isAuthenticated && (<button className="nav-item nav-link nav-pills" id="btn-login" onClick={() => logout()}>Cerrar Sesión</button>)}
+              </div>
+          </div>
+      </nav>
+      );
+    }
+
+}
+
+export default Navegacion;
