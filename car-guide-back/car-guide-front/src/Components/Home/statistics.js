d0c38782ddaa97111652de059fcb8f1d946fa5ee
Leidy Romero <lj.romero@uniandes.edu.co>
2019-11-03 14:24:44 -0500

    front home

--- car-guide-back/car-guide-front/src/Components/Home/statistics.js
+++ car-guide-back/car-guide-front/src/Components/Home/statistics.js
@@ -1,19 +1,6 @@
 import React, {Component} from "react";
 import "./statistics.css";
 class Home extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state= { 
-            carros:[],
-          };
-    }
-    get(){
-        fetch(
-           "/cars/" 
-        ).then((response)=>{response.json();
-        }).then(data=>this.setState({carros:data}))
-    }
     render()
     {
 
@@ -31,16 +18,16 @@ class Home extends Component {
             <div className="card-deck2 dashboard-background2">
                 <div className="data-card">
                 <h5 id="card-title">Más vendido</h5>
-                <p id="card-data">{this.state.carros[1].name}</p>
+                <p id="card-data">Chevrolet Captiva</p>
                 
                 </div>
                 <div className="data-card">
                 <h5 id="card-title">Marca más popular</h5>
-                <p id="card-data">{this.state.carros[2].name}</p>
+                <p id="card-data">Ford</p>
                 </div>
                 <div className="data-card">
                 <h5 id="card-title">Modelo más popular</h5>
-                <p id="card-data">{this.state.carros[3].name}</p>
+                <p id="card-data">Aveo</p>
                 </div>
                 <br></br>
             </div>
