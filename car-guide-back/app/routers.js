c4c2f440f572aabd5fd91487d6f36d7c61e47ebe
Juliana Prieto <jprietoarcila@gmail.com>
2019-11-04 17:56:21 -0500

    Services API

--- car-guide-back/app/routers.js
+++ car-guide-back/app/routers.js
@@ -2,12 +2,14 @@
 const router_auth = require('./authorization/authController');
 const router_users = require('./user/userController');
 const router_cars = require('./car/carController');
+const router_services = require('./service/serviceController')
 
 var routers = function(app) {
     
     app.use('/auth', router_auth);
     app.use('/users', router_users);
     app.use('/cars', router_cars);
+    app.use('/services', router_services);
     
 };
 
