da16703ba2a21edaeb416d582f09b94f8256831b
Juliana Prieto <jprietoarcila@gmail.com>
2019-11-04 18:52:50 -0500

    Sales added

--- car-guide-back/app/routers.js
+++ car-guide-back/app/routers.js
@@ -2,15 +2,15 @@
 const router_auth = require('./authorization/authController');
 const router_users = require('./user/userController');
 const router_cars = require('./car/carController');
-const router_services = require('./service/serviceController')
+const router_services = require('./service/serviceController');
+const router_sales = require('./sale/saleController');
 
 var routers = function(app) {
-    
     app.use('/auth', router_auth);
     app.use('/users', router_users);
     app.use('/cars', router_cars);
     app.use('/services', router_services);
-    
+    app.use('/sales', router_sales)
 };
 
 module.exports = routers;
