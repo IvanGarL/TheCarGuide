6bab2b75bc0c253d269926b9fe2d021cf9a6f199
Leidy Romero <lj.romero@uniandes.edu.co>
2019-11-05 06:56:58 -0500

    diseño cards users

--- car-guide-back/car-guide-front/src/Components/Users/user.js
+++ car-guide-back/car-guide-front/src/Components/Users/user.js
@@ -10,17 +10,17 @@ class User extends Component {
                 <div className="card space link">
                     <h5 className="card-title">{this.props.obj.name}</h5>
                     <div className="row">
-                        <div className="col-6">
+                        <div className="col-8">
                             <h6 id="card-sub-title">Correo: {this.props.obj.email}</h6>
                         </div>
-                        <div className="col-6">
-                            <h6 id="card-sub-title">Edad: {this.props.obj.age}</h6>   
+                        <div className="col-4">
+                            <h6 id="card-sub-title2">Edad: {this.props.obj.age}</h6>   
                         </div>
                         <div className="col-6">
                             <h6 id="card-sub-title">Número: {this.props.obj.phone}</h6>   
                         </div>
                         <div className="col-6">
-                            <h6 id="card-sub-title">Tipo: {this.props.obj.role}</h6>   
+                            <h6 id="card-sub-title2">Tipo: {this.props.obj.role}</h6>   
                         </div>
                     </div>
                 </div>
