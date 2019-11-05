3816a164a777a3fe755a59728bd1c343bd202f00
Pablo Suarez <pa.suarezm@uniandes.edu.co>
2019-11-05 09:53:15 -0500

    Arreglos de diseño

--- car-guide-back/car-guide-front/src/Components/Services/service.js
+++ car-guide-back/car-guide-front/src/Components/Services/service.js
@@ -7,18 +7,26 @@ class Service extends Component {
             <div className="data-card servicios-card">
                 <div className="row">
                     <div className="col-3">
-                        <img className="card-img-top" width="50" src={`${this.props.service.image}`}/>
+                        <img className="card-img-top" width="50" src={`${this.props.service.image}`} alt="banner servicios"/>
                     </div>
                     <div className="col-1"></div>
                     <div className="col-7">
-                        <div className="row text-left">
-                            <h5 id="card-title">{this.props.service.name}</h5>
+                        <div className="row">
+                            <div className="col-12 text-left">
+                                <h5 id="card-title">{this.props.service.name}</h5>
+                            </div>
                         </div>
                         <div className="row text-left">
-                            <p>{this.props.service.description}</p>
+                            <div className="col-12">
+                                <p>{this.props.service.description}</p>
+                            </div>
                         </div>
-                        <div className="row bg-danger text-light">
-                            <p className="text-align"><b>$ {this.props.service.price} COP </b></p>
+                        <div className="row text-light">
+                            <div className="col-4"></div>
+                            <div className="col-4 bg-danger">    
+                                <p className="text-center"><b>$ {this.props.service.price} COP </b></p>
+                            </div>
+                            <div className="col-4"></div>
                         </div>
                     </div>
                     <div className="col-1"></div>
