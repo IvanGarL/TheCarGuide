2a1bb495ff0a39649ce77ede8d0621084f319983
Leidy Romero <lj.romero@uniandes.edu.co>
2019-11-05 09:33:53 -0500

    axe corrections

--- car-guide-back/car-guide-front/src/Components/Users/user.js
+++ car-guide-back/car-guide-front/src/Components/Users/user.js
@@ -11,16 +11,16 @@ class User extends Component {
                     <h5 className="card-title">{this.props.obj.name}</h5>
                     <div className="row">
                         <div className="col-8">
-                            <h6 id="card-sub-title">Correo: {this.props.obj.email}</h6>
+                            <h6 className="card-sub-title">Correo: {this.props.obj.email}</h6>
                         </div>
                         <div className="col-4">
-                            <h6 id="card-sub-title2">Edad: {this.props.obj.age}</h6>   
+                            <h6 className="card-sub-title2">Edad: {this.props.obj.age}</h6>   
                         </div>
-                        <div className="col-6">
-                            <h6 id="card-sub-title">Número: {this.props.obj.phone}</h6>   
+                        <div className="col-8">
+                            <h6 className="card-sub-title">Número: {this.props.obj.phone}</h6>   
                         </div>
-                        <div className="col-6">
-                            <h6 id="card-sub-title2">Tipo: {this.props.obj.role}</h6>   
+                        <div className="col-4">
+                            <h6 className="card-sub-title2">Tipo: {this.props.obj.role}</h6>   
                         </div>
                     </div>
                 </div>
