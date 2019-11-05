613679c1e04d0ad1d688f3f3342fdd5af7f31876
Leidy Romero <lj.romero@uniandes.edu.co>
2019-11-05 09:43:41 -0500

    axe corrections

--- car-guide-back/car-guide-front/src/Components/Home/statistics.js
+++ car-guide-back/car-guide-front/src/Components/Home/statistics.js
@@ -26,23 +26,23 @@ class Home extends Component {
                 <div className="row dashboard-background2">
                     <div className="col-4">
                         <div className="card space link">
-                        <h5 id="card-title"><b>Más vendido</b></h5>
+                        <h5 className="card-title"><b>Más vendido</b></h5>
                         <img className="card-img-top" width="50" height="200" src={`${this.state.carros[4].image}`} alt="Card image cap"></img>
-                        <p id="card-sub-title2">{this.state.carros[4].name}</p> 
+                        <p className="card-sub-title2">{this.state.carros[4].name}</p> 
                         </div>
                     </div>
                     <div className="col-4">
                         <div className="card space link">
-                        <h5 id="card-title"><b>Marca más popular</b></h5>
+                        <h5 className="card-title"><b>Marca más popular</b></h5>
                         <img className="card-img-top" width="50" height="200" src={`${this.state.carros[2].image}`} alt="Card image cap"></img>
-                        <p id="card-sub-title2">{this.state.carros[2].brand}</p>
+                        <p className="card-sub-title2">{this.state.carros[2].brand}</p>
                         </div>
                     </div>
                     <div className="col-4">
                         <div className="card space link">
-                        <h5 id="card-title"><b>Modelo más popular</b></h5>
+                        <h5 className="card-title"><b>Modelo más popular</b></h5>
                         <img className="card-img-top" width="50" height="200" src={`${this.state.carros[3].image}`} alt="Card image cap"></img>
-                        <p id="card-sub-title2">{this.state.carros[3].model}</p>
+                        <p className="card-sub-title2">{this.state.carros[3].model}</p>
                         
                         </div>
                     </div>
